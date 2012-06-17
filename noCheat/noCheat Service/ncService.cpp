c1538b5182dcd3ff9e34541125fef36e73817824
Qix <qix@lavabit.com>
2012-06-16 19:42:24 -0700

    Closed #51: Created base service

--- noCheat/noCheat Service/ncService.cpp
+++ noCheat/noCheat Service/ncService.cpp
@@ -1,3 +1,15 @@
+/************************************************************************/
+/*                                                                      */
+/*    noCheatS                                                          */
+/*       The official noCheat Service Process                           */
+/*                                                                      */
+/*       This process runs the heavy-duty backend tasks that operate    */
+/*       on the end-user's PC. This service communcates not only with   */
+/*       the driver on the computer, but with the front-end appli-      */
+/*       cation and the website for noCheat (noCheatW).                 */
+/*                                                                      */
+/************************************************************************/
+
 #include <windows.h>
 
 int main()
